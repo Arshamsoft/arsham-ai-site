fb110dafaff79f1878ba81daad6d17983a2dec69
Arsham <arshampowerful63@gmail.com>
2025-08-25 14:20:09 +0330

    8

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Portfolio from './pages/Portfolio'
 import Shop from './pages/Shop'
 import About from './pages/About'
 import Contact from './pages/Contact'
+import LanguageSwitcher from './LanguageSwitcher'
+
 
 function App() {
   return (
