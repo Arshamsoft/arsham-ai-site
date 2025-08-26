16b262fb0a09a2135cff6564ccaa64551f52a381
Arsham <arshampowerful63@gmail.com>
2025-08-26 23:25:57 +0330

    28

--- src/context/LanguageContext.js
+++ src/context/LanguageContext.js
@@ -3,7 +3,7 @@ import { createContext, useState } from 'react'
 export const LanguageContext = createContext()
 
 export function LanguageProvider({ children }) {
-  const [lang, setLang] = useState('en') // زبان پیش‌فرض
+  const [lang, setLang] = useState('fa') // یا 'en'
 
   return (
     <LanguageContext.Provider value={{ lang, setLang }}>
